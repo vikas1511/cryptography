ab44568a2fc7ee1a180a45421a7920b43937258e
Vikas M <vikas1511@hotmail.com>
2018-03-06 22:33:16 +0530

    changes to text.

--- projectTrial2/projectTrial2/Form1.Designer.cs
+++ projectTrial2/projectTrial2/Form1.Designer.cs
@@ -51,7 +51,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "browse";
+            this.button1.Text = "Browse";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
