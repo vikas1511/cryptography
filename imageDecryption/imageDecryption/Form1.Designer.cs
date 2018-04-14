bc04fed7938d25df3fefbefadec84455c5f346f6
Vikas M <vikas1511@hotmail.com>
2018-04-14 12:39:25 +0530

    minor design changes

--- imageDecryption/imageDecryption/Form1.Designer.cs
+++ imageDecryption/imageDecryption/Form1.Designer.cs
@@ -95,6 +95,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(16, 129);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(540, 455);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
