29209a01b8746d33fcf9c7e41dd98973830fe148
veeresh udamnalli <veeresh.udamnalli@gmail.com>
2018-04-17 17:08:34 +0530

    new1

--- encryptionInterface/encryptionInterface/txtency.Designer.cs
+++ encryptionInterface/encryptionInterface/txtency.Designer.cs
@@ -47,6 +47,7 @@
             this.textBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             resources.ApplyResources(this.textBox1, "textBox1");
             this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
--- encryptionInterface/encryptionInterface/txtency.cs
+++ encryptionInterface/encryptionInterface/txtency.cs
@@ -41,5 +41,10 @@ namespace encryptionInterface
         {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
