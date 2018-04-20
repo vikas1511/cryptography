3ecbb3efc14b9d89e322357ed3943dfeaf31c6dd
Yashwanth Eshwar <yashwanth.eshwar@gmail.com>
2018-04-20 23:56:55 +0530

    Added ne w Welcome window, named it Securo. Check it once it leads to login page.

--- encryptionInterface/encryptionInterface/Welcome.Designer.cs
+++ encryptionInterface/encryptionInterface/Welcome.Designer.cs
@@ -65,7 +65,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Lucida Handwriting", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(441, 34);
+            this.label1.Location = new System.Drawing.Point(441, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(244, 63);
             this.label1.TabIndex = 2;
